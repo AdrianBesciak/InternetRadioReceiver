640aa870be3821947d9f98c14ab1ddfe26dd123e
Adrian Beściak <adrian.besciak99@gmail.com>
2022-01-05 16:28:55 +0100

    new radio stations

--- Radio/audio/playlist/InternalRadioPlaylist.cpp
+++ Radio/audio/playlist/InternalRadioPlaylist.cpp
@@ -4,17 +4,15 @@ namespace audio {
     audio::InternalRadioPlaylist::InternalRadioPlaylist()
             : internalEntries({
               Entry("RMF FM", "http://195.150.20.4/rmf_fm"),
-              Entry("RMF MAXX", "http://195.150.20.4/rmf_fm"),
-              Entry("Eska", "http://195.150.20.4/rmf_fm"),
-              Entry("Radio Krakow", "http://195.150.20.4/rmf_fm"),
-              Entry("Radio Maryja", "http://195.150.20.4/rmf_fm"),
-              Entry("RDN", "http://195.150.20.4/rmf_fm"),
-              Entry("Radio ZET", "http://195.150.20.4/rmf_fm"),
-              Entry("Radio 357", "http://195.150.20.4/rmf_fm"),
-              Entry("MINE", "http://195.150.20.4/rmf_fm"),
-              Entry("MINE2", "http://195.150.20.4/rmf_fm"),
-              Entry("MINE3", "http://195.150.20.4/rmf_fm")
-            }) {}
+              Entry("RMF MAXX", "http://217.74.72.12/rmf_maxxx"),
+              Entry("Eska Bialystok", "http://waw01-02.ic.smcdn.pl:8000/2020-1.mp3"),
+              Entry("Radio Poznan", "http://stream4.nadaje.com:8578/poznan"),
+              Entry("Radio Warszawa", "http://stream2.technologicznie.net:80/radio_warszawa.mp3"),
+              Entry("RDN", "http://stream1.dabcom.pl:8000/rdn_mpl"),
+              Entry("Antyradio", "http://redir.atmcdn.pl/sc/o2/Eurozet/live/antyradio.livx?audio=5"),
+              Entry("Radio Kielce", "http://gra.radio.kielce.pl:8000/rk2"),
+              Entry("Radio Plus Lublin", "http://waw01-02.ic.smcdn.pl:8000/4130-1.mp3")
+      }) {}
 
     const std::vector<Playlist::Entry> &InternalRadioPlaylist::getEntries() const {
         return internalEntries;
